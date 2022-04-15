cc9b374f1c7f2b66aa811de9729798d97e3ef8a0
Peeter Liik <liikpeeter@gmail.com>
2022-04-15 12:23:28 +0300

    add endpoints

---
+++ src/controllers/endpoints.js
@@ -0,0 +1,48 @@
+import fetch from "node-fetch";
+
+const url = "localhost:3000"; //TO-DO Update when service up
+
+export const getConsumptionCurrentMonth = async () => {
+	const response = await fetch(url + "/consumption/month");
+	const data = await response.text();
+	return data;
+};
+
+export const getConsumptionMonth = async (month, year) => {
+	const response = await fetch(
+		url + "consumption/month?month=" + month + "&year=" + year
+	);
+	const data = await response.text();
+	return data;
+};
+
+export const getLatest = async () => {
+	const response = await fetch(url + "/readings/latest");
+	const data = await response.json();
+	return data;
+};
+
+export const getReadingCurrentMonth = async () => {
+	const response = await fetch(url + "/readingmonth");
+	const data = await response.json();
+	return data;
+};
+
+export const getReadingMonth = async (month, year) => {
+	const response = await fetch(
+		url + "/readindmonth?month=" + month + "&year=" + year
+	);
+	const data = await response.json();
+	return data;
+};
+
+//body = { id?: _id, email: _email, value?: _value, month?: _month }
+export const putSendReading = async (payload) => {
+	const response = await fetch(url + "/send", {
+		method: "PATCH",
+		body: JSON.stringify(payload),
+		headers: { "Content-Type": "application/json" },
+	});
+	const data = await response.json();
+	return data;
+};
